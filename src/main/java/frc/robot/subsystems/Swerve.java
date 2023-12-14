e4d1e0a8e6add83e4d667d6fc0bff5445120ae89
Mitchell <mgresham4@ivytech.edu>
2023-12-13 20:11:23 -0500

    Error Building (Cache Issue?)

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -78,7 +78,7 @@ public class Swerve extends SubsystemBase {
     }
 
     public void resetOdometry(Pose2d pose) {
-        swerveOdometry.resetPosition(getYaw(), getModulePositions(), pose);
+        swerveOdometry.resetPosition(pose, getYaw(), getModulePositions());
     }
 
     public SwerveModuleState[] getModuleStates(){
