06c97a37f5f274aeac55afecac2dcd74474e36d5
Mitchell <mgresham4@ivytech.edu>
2023-12-20 20:56:36 -0500

    Implemented Optional Git Versioning Plugin

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2023SwerveTest";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 53;
+  public static final String GIT_SHA = "99e7ff6341266b94ad73dfb1a08445ba3e9fd207";
+  public static final String GIT_DATE = "2023-12-20 20:52:03 EST";
+  public static final String GIT_BRANCH = "advantage_plus";
+  public static final String BUILD_DATE = "2023-12-20 20:54:48 EST";
+  public static final long BUILD_UNIX_TIME = 1703123688109L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
